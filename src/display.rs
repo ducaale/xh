9b40d3114d1ee5a0623a23abf3918e3d0e68f612
ducaale <sharaf.13@hotmail.com>
2020-09-23 20:28:56 +0100

    copy httpie theme

--- src/display.rs
+++ src/display.rs
@@ -1,19 +1,55 @@
 use std::fmt::Write;
 
 use crate::Pretty;
+use ansi_term;
+use ansi_term::Color::{self, Fixed, RGB};
 use reqwest::blocking::{Request, Response};
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use serde::Serialize;
 use serde_json::Value;
 use syntect::easy::HighlightLines;
-use syntect::highlighting::{Style, ThemeSet};
+use syntect::highlighting::ThemeSet;
 use syntect::parsing::{SyntaxSet, SyntaxSetBuilder};
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
+use syntect::util::LinesWithEndings;
+
+// https://github.com/sharkdp/bat/blob/3a85fd767bd1f03debd0a60ac5bc08548f95bc9d/src/terminal.rs
+fn to_ansi_color(color: syntect::highlighting::Color) -> ansi_term::Color {
+    if color.a == 0 {
+        // Themes can specify one of the user-configurable terminal colors by
+        // encoding them as #RRGGBBAA with AA set to 00 (transparent) and RR set
+        // to the 8-bit color palette number. The built-in themes ansi-light,
+        // ansi-dark, base16, and base16-256 use this.
+        match color.r {
+            // For the first 8 colors, use the Color enum to produce ANSI escape
+            // sequences using codes 30-37 (foreground) and 40-47 (background).
+            // For example, red foreground is \x1b[31m. This works on terminals
+            // without 256-color support.
+            0x00 => Color::Black,
+            0x01 => Color::Red,
+            0x02 => Color::Green,
+            0x03 => Color::Yellow,
+            0x04 => Color::Blue,
+            0x05 => Color::Purple,
+            0x06 => Color::Cyan,
+            0x07 => Color::White,
+            // For all other colors, use Fixed to produce escape sequences using
+            // codes 38;5 (foreground) and 48;5 (background). For example,
+            // bright red foreground is \x1b[38;5;9m. This only works on
+            // terminals with 256-color support.
+            //
+            // TODO: When ansi_term adds support for bright variants using codes
+            // 90-97 (foreground) and 100-107 (background), we should use those
+            // for values 0x08 to 0x0f and only use Fixed for 0x10 to 0xff.
+            n => Fixed(n),
+        }
+    } else {
+        RGB(color.r, color.g, color.b)
+    }
+}
 
 fn colorize<'a>(text: &'a str, syntax: &str) -> impl Iterator<Item = String> + 'a {
     lazy_static! {
-        // static ref PS: SyntaxSet = SyntaxSet::load_defaults_newlines();
-        static ref TS: ThemeSet = ThemeSet::load_defaults();
+        static ref TS: ThemeSet = ThemeSet::load_from_folder("assets").unwrap();
         static ref PS: SyntaxSet = {
             let mut ps = SyntaxSetBuilder::new();
             ps.add_from_folder("assets", true).unwrap();
@@ -21,10 +57,15 @@ fn colorize<'a>(text: &'a str, syntax: &str) -> impl Iterator<Item = String> + '
         };
     }
     let syntax = PS.find_syntax_by_extension(syntax).unwrap();
-    let mut h = HighlightLines::new(syntax, &TS.themes["Solarized (dark)"]);
+    let mut h = HighlightLines::new(syntax, &TS.themes["ansi"]);
     LinesWithEndings::from(text).map(move |line| {
-        let ranges: Vec<(Style, &str)> = h.highlight(line, &PS);
-        as_24_bit_terminal_escaped(&ranges[..], false)
+        let mut s: String = String::new();
+        let highlights = h.highlight(line, &PS);
+        for (style, component) in highlights {
+            let color = to_ansi_color(style.foreground);
+            write!(s, "{}", &color.paint(component)).unwrap();
+        }
+        s
     })
 }
 
@@ -128,11 +169,14 @@ pub fn print_body(body: Box<dyn FnOnce() -> String>, content_type: Option<&str>,
             println!("+-----------------------------------------+");
             print!("\n\n");
         } else if content_type.contains("json") {
-            print_json(&body(), &pretty)
+            print_json(&body(), &pretty);
         } else if content_type.contains("xml") {
-            print_xml(&body(), &pretty)
+            print_xml(&body(), &pretty);
         } else if content_type.contains("html") {
-            print_html(&body(), &pretty)
+            print_html(&body(), &pretty);
+        } else {
+            println!("{}", &body());
         }
     }
+    print!("\n");
 }
