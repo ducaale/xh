6c5073a0825a8db1045af20a66bc9bb0c14c6c4d
ducaale <sharaf.13@hotmail.com>
2020-12-04 23:39:30 +0000

    experiment with testing the cli

---
+++ tests/cli.rs
@@ -0,0 +1,34 @@
+use assert_cmd::prelude::*;
+use indoc::indoc;
+use std::process::Command;
+
+#[test]
+fn basic_post() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("yahc")?;
+    cmd.arg("-v")
+        .arg("--offline")
+        .arg("--ignore-stdin")
+        .arg("--pretty=format")
+        .arg("post")
+        .arg("httpbin.org/post")
+        .arg("name=ali");
+
+    cmd.assert().stdout(indoc! {r#"
+
+        POST /post HTTP/1.1
+        accept: */*
+        accept: application/json, */*
+        accept-encoding: gzip, deflate
+        connection: keep-alive
+        content-length: 14
+        content-type: application/json
+        host: httpbin.org
+
+        {
+            "name": "ali"
+        }
+
+    "#});
+
+    Ok(())
+}
