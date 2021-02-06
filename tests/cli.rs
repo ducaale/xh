24881769892da3d8400c7c6821ef225b268ad88e
Till H <6740568+Till--H@users.noreply.github.com>
2021-02-06 11:16:07 +0100

    Add support for HEAD
    - Support HEAD requests

--- tests/cli.rs
+++ tests/cli.rs
@@ -30,3 +30,48 @@ fn basic_post() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn basic_get() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ht")?;
+    cmd.arg("-v")
+        .arg("--offline")
+        .arg("--ignore-stdin")
+        .arg("--pretty=format")
+        .arg("get")
+        .arg("httpbin.org/get");
+
+    cmd.assert().stdout(indoc! {r#"
+        GET /get HTTP/1.1
+        accept: */*
+        accept-encoding: gzip, deflate
+        connection: keep-alive
+        host: httpbin.org
+
+    "#});
+
+    Ok(())
+}
+
+#[test]
+fn basic_head() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ht")?;
+    cmd.arg("-v")
+        .arg("--offline")
+        .arg("--ignore-stdin")
+        .arg("--pretty=format")
+        .arg("head")
+        .arg("httpbin.org/get");
+
+    cmd.assert().stdout(indoc! {r#"
+        HEAD /get HTTP/1.1
+        accept: */*
+        accept-encoding: gzip, deflate
+        connection: keep-alive
+        host: httpbin.org
+
+    "#});
+
+    Ok(())
+}
+
