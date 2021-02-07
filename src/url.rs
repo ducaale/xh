de819e7e4f828f766d27c975f8a0135328e10680
ducaale <sharaf.13@hotmail.com>
2021-02-07 12:32:24 +0000

    run cargo clippy --fix

--- src/url.rs
+++ src/url.rs
@@ -6,12 +6,12 @@ impl Url {
     pub fn new(url: String, default_scheme: Option<String>) -> Url {
         let default_scheme = default_scheme.unwrap_or("http://".to_string());
         let re = Regex::new("[a-zA-Z]://.+").unwrap();
-        if url.starts_with(":") {
+        if url.starts_with(':') {
             let url = format!("{}{}{}", default_scheme, "localhost", url);
-            Url(reqwest::Url::parse(&url).unwrap().into())
+            Url(reqwest::Url::parse(&url).unwrap())
         } else if !re.is_match(&url) {
             let url = format!("{}{}", default_scheme, url);
-            Url(reqwest::Url::parse(&url).unwrap().into())
+            Url(reqwest::Url::parse(&url).unwrap())
         } else {
             Url(reqwest::Url::parse(&url).unwrap())
         }
