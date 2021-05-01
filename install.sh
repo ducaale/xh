f75d05469c1bd8a7c151b10c8f3b738fd6f946be
ducaale <sharaf.13@hotmail.com>
2021-05-01 16:08:20 +0100

    list all the locations xh is installed to

--- install.sh
+++ install.sh
@@ -78,4 +78,6 @@ else
     sudo ln -sf "$bindir/xh" "$bindir/xhs"
 fi
 
-echo "$("$bindir"/xh --version) has been installed to $bindir/xh"
+echo "$("$bindir"/xh --version) has been installed to:"
+echo " • $bindir/xh"
+echo " • $bindir/xhs"
