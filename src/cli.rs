76d822540ba8d2a821634ad07bd92571b3e6b475
ducaale <sharaf.13@hotmail.com>
2022-01-01 20:18:56 +0200

    update clap guidelines

--- src/cli.rs
+++ src/cli.rs
@@ -27,7 +27,6 @@ use crate::utils::config_dir;
 // - Only use `long` with an implicit arg (just `long`)
 //   - Unless it needs a different name, but then also use `name = "..."`
 // - Add an uppercase value_name to options that take a value
-// - Add a line with triple {n} after any long doc comment
 
 /// xh is a friendly and fast tool for sending HTTP requests.
 ///
