b919bb62f5d7bb056f609237706ea9db63827b00
Jan Verbeek <jan.verbeek@posteo.nl>
2023-11-01 19:39:09 +0100

    install.sh: Print curl error message on server error

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 fetch()
 {
     if which curl > /dev/null; then
-        if [ "$#" -eq 2 ]; then curl -L -o "$1" "$2"; else curl -sSL "$1"; fi
+        if [ "$#" -eq 2 ]; then curl -fL -o "$1" "$2"; else curl -fsSL "$1"; fi
     elif which wget > /dev/null; then
         if [ "$#" -eq 2 ]; then wget -O "$1" "$2"; else wget -nv -O - "$1"; fi
     else
