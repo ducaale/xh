652e3df82c7abad436ba440a7ef8af47874ae4b3
Mohamed Daahir <sharaf.13@hotmail.com>
2024-12-30 10:44:21 +0200

    add test for cookies

--- tests/cases/http_unix.rs
+++ tests/cases/http_unix.rs
@@ -70,10 +70,115 @@ fn redirects_stay_on_same_server() {
             server.socket_path().to_string_lossy()
         ))
         .arg("--follow")
+        .arg("--verbose")
+        .arg("--all")
         .assert()
-        .success();
+        .stdout(indoc! {r#"
+            GET /first_page HTTP/1.1
+            Accept: */*
+            Accept-Encoding: gzip, deflate, br, zstd
+            Connection: keep-alive
+            Host: http.mock
+            User-Agent: xh/0.0.0 (test mode)
+
+            HTTP/1.1 302 Found
+            Content-Length: 14
+            Date: N/A
+            Location: http://localhost:8000/second_page
+
+            redirecting...
+
+            GET /second_page HTTP/1.1
+            Accept: */*
+            Accept-Encoding: gzip, deflate, br, zstd
+            Connection: keep-alive
+            Host: http.mock
+            User-Agent: xh/0.0.0 (test mode)
+
+            HTTP/1.1 302 Found
+            Content-Length: 14
+            Date: N/A
+            Location: /third_page
+
+            redirecting...
+
+            GET /third_page HTTP/1.1
+            Accept: */*
+            Accept-Encoding: gzip, deflate, br, zstd
+            Connection: keep-alive
+            Host: http.mock
+            User-Agent: xh/0.0.0 (test mode)
+
+            HTTP/1.1 200 OK
+            Content-Length: 17
+            Date: N/A
+
+            final destination
+        "#});
 
     server.assert_hits(3);
 }
 
-// TODO: add tests for cookies
+#[cfg(target_family = "unix")]
+#[test]
+fn cookies_persist_across_redirects() {
+    let server = server::http_unix(|req| async move {
+        match req.uri().path() {
+            "/first_page" => hyper::Response::builder()
+                .status(302)
+                .header("Date", "N/A")
+                .header("Location", "/second_page")
+                .header("set-cookie", "hello=world")
+                .body("redirecting...".into())
+                .unwrap(),
+            "/second_page" => hyper::Response::builder()
+                .header("Date", "N/A")
+                .body("final destination".into())
+                .unwrap(),
+            _ => panic!("unknown path"),
+        }
+    });
+
+    get_command()
+        .arg("localhost:3000/first_page")
+        .arg(format!(
+            "--unix-socket={}",
+            server.socket_path().to_string_lossy()
+        ))
+        .arg("--follow")
+        .arg("--verbose")
+        .arg("--all")
+        .assert()
+        .stdout(indoc! {r#"
+            GET /first_page HTTP/1.1
+            Accept: */*
+            Accept-Encoding: gzip, deflate, br, zstd
+            Connection: keep-alive
+            Host: http.mock
+            User-Agent: xh/0.0.0 (test mode)
+
+            HTTP/1.1 302 Found
+            Content-Length: 14
+            Date: N/A
+            Location: /second_page
+            Set-Cookie: hello=world
+
+            redirecting...
+
+            GET /second_page HTTP/1.1
+            Accept: */*
+            Accept-Encoding: gzip, deflate, br, zstd
+            Connection: keep-alive
+            Cookie: hello=world
+            Host: http.mock
+            User-Agent: xh/0.0.0 (test mode)
+
+            HTTP/1.1 200 OK
+            Content-Length: 17
+            Date: N/A
+
+            final destination
+        "#});
+}
+
+// TODO: add tests for connection timeout
